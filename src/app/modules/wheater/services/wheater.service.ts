6fe0fcfce84204e33404f6997cba1b6f3bed25c6
Franklin Almeida Campos <franklindrw@gmail.com>
2024-03-20 21:23:58 -0300

    initial commit

---
+++ src/app/modules/wheater/services/wheater.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WheaterService {
+  private apiKey = environment.API_KEY;
+  private apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(city: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}?q=${city}&units=metric&mode=json&appid=${this.apiKey}`);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_KEY: "adbbddb8c1dfa522de3d23eee4891b26",
+  API_URL: "https://api.openweathermap.org/data/2.5/weather"
+}
+
