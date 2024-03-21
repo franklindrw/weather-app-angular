3a9897e716f2aa06e18a30f2d28fdf1952078d63
Franklin Almeida Campos <franklindrw@gmail.com>
2024-03-20 21:45:36 -0300

    initial commit

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false, // Set to true if you are in production
+  API_KEY: "", // Your API Key from OpenWeatherMap
+  API_URL: "https://api.openweathermap.org/data/2.5/weather"
+}
+
